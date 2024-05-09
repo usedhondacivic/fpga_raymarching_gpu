c92693a5dd86117dbff0af1e4a60a6dd804e5a97
Michael Miller Crum <mmc323@cornell.edu>
2024-05-09 19:13:28 -0400

    Glitchy infinite

--- verilator/raymarcher.v
+++ verilator/raymarcher.v
@@ -309,7 +309,7 @@ rayInfo raymarch() {
 }
 */
 module raymarcher #(
-    parameter ITR_PER_LOOP = 4,
+    parameter ITR_PER_LOOP = 3,
     parameter FRAG_DIR_PIPELINE_CYCLES = 8,
     parameter PIPELINE_ARR_SIZE = ITR_PER_LOOP + FRAG_DIR_PIPELINE_CYCLES
 ) (
